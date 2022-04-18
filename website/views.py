d3371d2cc0d313af1cdd368da0a6183773223c1a
mehrbod <mehrbodjavadi79@gmail.com>
2022-04-19 00:46:36 +0430

    add staff view set

--- website/views.py
+++ website/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.generics import GenericAPIView
 
-# Create your views here.
+from models import Staff
+from serializers import StaffSerializer
+from permissions import AdminWritePermission
+
+
+class StaffsListViewSet(
+    GenericAPIView,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
+    queryset = Staff.objects.all()
+    serializer_class = StaffSerializer
+    permission_classes = AdminWritePermission
