ab54be849ae2ce50341c80172234811831b8ec8d
mehrbod <mehrbodjavadi79@gmail.com>
2022-04-19 00:46:48 +0430

    admin permission

---
+++ permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class AdminWritePermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return False  # todo for admin must be true
