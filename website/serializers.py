7c04f49952f7fca4deaac59379736c260934a7e0
mehrbod <mehrbodjavadi79@gmail.com>
2022-04-19 00:46:14 +0430

    add staff serializer

---
+++ website/serializers.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+from rest_framework import serializers
+
+from .models import Staff
+
+
+class StaffSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Staff
+        fields = ['__all__', ]
+        exclude = ('id', )
