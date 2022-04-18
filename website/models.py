c5913a06bc83c7c1367e6fe0c255abba0d04045c
mehrbod <mehrbodjavadi79@gmail.com>
2022-04-19 00:46:01 +0430

    add staff model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from constants import SHORT_TEXT_MAX_LENGTH, URL_MAX_LENGTH
 
-# Create your models here.
+
+def staff_upload_path(self, filename):
+    return f'staff/{self.group_title}/{self.team_title}/{filename}'
+
+
+class Staff(models.Model):
+    group_title = models.CharField(max_length=SHORT_TEXT_MAX_LENGTH)
+    team_title = models.CharField(max_length=SHORT_TEXT_MAX_LENGTH, blank=True, null=True)
+    first_name_en = models.CharField(max_length=SHORT_TEXT_MAX_LENGTH, null=True, blank=True)
+    first_name_fa = models.CharField(max_length=SHORT_TEXT_MAX_LENGTH)
+    last_name_en = models.CharField(max_length=SHORT_TEXT_MAX_LENGTH, null=True, blank=True)
+    last_name_fa = models.CharField(max_length=SHORT_TEXT_MAX_LENGTH)
+    role = models.CharField(max_length=SHORT_TEXT_MAX_LENGTH, blank=True, null=False)
+    url = models.CharField(max_length=URL_MAX_LENGTH, null=True, blank=True)
+    image = models.ImageField(upload_to=staff_upload_path)
