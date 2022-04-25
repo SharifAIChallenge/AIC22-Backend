da44e071a4afd27fc2b42551f9739ea33077a7b1
Mohammad Broughani <mbroughani81@gmail.com>
2022-04-25 10:36:51 +0430

    add tweet serializer

--- website/serializers.py
+++ website/serializers.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 from rest_framework import serializers
 
-from .models import Staff
+from .models import Staff, Tweet
 
 
 class StaffSerializer(serializers.ModelSerializer):
@@ -10,3 +10,9 @@ class StaffSerializer(serializers.ModelSerializer):
     class Meta:
         model = Staff
         exclude = ('id', )
+
+
+class TweetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tweet
+        exclude = ('id',)
