746bba2cab9cf24967d4b598bf9383148539be0b
mehrbod <mehrbodjavadi79@gmail.com>
2022-04-28 20:01:50 +0430

    edit swagger url

--- AIC22_Backend/urls.py
+++ AIC22_Backend/urls.py
@@ -36,5 +36,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(AIC_v1_router.urls)),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^api-doc/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
