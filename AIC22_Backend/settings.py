d2e561fd557ee873818e7f22726855837d093231
ar-hosseinkhani <alireza.8096@chmail.ir>
2022-04-29 15:29:07 +0430

    change order of installed apps!

--- AIC22_Backend/settings.py
+++ AIC22_Backend/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'account',
     'website',
-    'rest_framework',
     'drf_yasg',
-    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
