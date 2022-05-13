6b9719dbcb0c64164ca814326494f9debe710207
mhbahmani <mhbahmani79@gmail.com>
2022-05-14 01:09:57 +0430

    Add static and media root

--- AIC22_Backend/settings.py
+++ AIC22_Backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,6 +135,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'static_files'))
+
+MEDIA_ROOT = (
+    os.environ.get('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
