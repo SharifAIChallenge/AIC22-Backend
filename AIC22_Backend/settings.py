f54cc33b9845a9d007a46a2d98d7f5f1901ce999
mehrbod <mehrbodjavadi79@gmail.com>
2022-05-15 15:36:48 +0430

    fix flake8

--- AIC22_Backend/settings.py
+++ AIC22_Backend/settings.py
@@ -81,12 +81,13 @@ WSGI_APPLICATION = 'AIC22_Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': str(BASE_DIR / "db.sqlite3"),
 #    }
-#}
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
