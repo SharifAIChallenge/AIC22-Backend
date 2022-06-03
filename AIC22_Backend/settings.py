c0ac76e28ce4b7db4e38e473802a6191ef2cf542
mehrbod <mehrbodjavadi79@gmail.com>
2022-06-03 19:39:54 +0430

    fix template settings

--- AIC22_Backend/settings.py
+++ AIC22_Backend/settings.py
@@ -67,7 +67,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'position', 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
