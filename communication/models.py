971b08764cc87db38e2cf82b338598b7ebfcb3c4
Mohammad Broughani <gitmbroughani81@gmail.com>
2022-06-04 14:07:25 +0430

    add ticket model

--- communication/models.py
+++ communication/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+from constants import SHORT_TEXT_MAX_LENGTH
+
+
+class TicketStatus:
+    OPEN = 'open'
+    CLOSED = 'closed'
+
+    TYPES = (
+        (OPEN, OPEN),
+        (CLOSED, CLOSED),
+    )
+
+
+class ReplyStatus:
+    PENDING = 'pending'
+    ANSWERED = 'answered'
+
+    TYPES = (
+        (PENDING, PENDING),
+        (ANSWERED, ANSWERED),
+    )
+
+
+class Ticket(models.Model):
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='tickets'
+    )
+
+    tag = models.ForeignKey(
+        to='communication.Tag',
+        related_name='tickets',
+        on_delete=models.DO_NOTHING
+    )
+    title = models.CharField(max_length=SHORT_TEXT_MAX_LENGTH)
+    text = models.TextField()
+
+    is_public = models.BooleanField(default=False)
+
+    status = models.CharField(
+        max_length=SHORT_TEXT_MAX_LENGTH,
+        default=TicketStatus.OPEN,
+        choices=TicketStatus.TYPES
+    )
+
+    def __str__(self):
+        return f'{self.title} {self.author.username}'
+
+
+class Reply(models.Model):
+    ticket = models.ForeignKey(
+        to=Ticket,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='ticket_replies'
+    )
+    text = models.TextField()
+
+    status = models.CharField(
+        max_length=SHORT_TEXT_MAX_LENGTH,
+        default=ReplyStatus.PENDING,
+        choices=ReplyStatus.TYPES
+    )
+
+    def __str__(self):
+        return f'{self.user}'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=SHORT_TEXT_MAX_LENGTH)
+
+    def __str__(self):
+        return f'{self.title}'
