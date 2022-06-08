bd5274a168ef63d4a2991c92c095f37b4f191c96
Mohammad Broughani <gitmbroughani81@gmail.com>
2022-06-08 20:01:32 +0430

    add serializers

---
+++ challenge/serializers/map.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from challenge.models.map import Map
+
+
+class MapSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Map
+        fields = ('id', 'name', 'active', 'file')
