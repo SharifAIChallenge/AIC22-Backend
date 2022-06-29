78b5edf6d7766ad8d860aee5e3366f00b1f76d72
mehrbod <mehrbodjavadi79@gmail.com>
2022-06-30 02:02:51 +0430

    fix register email template

--- utils.py
+++ utils.py
@@ -6,14 +6,14 @@ class ImageURL:
         if not obj.image:
             return None
         path = obj.image.url
-        if settings.AIC_DOMAIN not in path:
-            return settings.AIC_DOMAIN + path
+        if settings.AIC_BACKEND_DOMAIN not in path:
+            return settings.AIC_BACKEND_DOMAIN + path
         return path
 
     def _resume_url(self, obj):
         if not obj.resume:
             return None
         path = obj.resume.url
-        if settings.AIC_DOMAIN not in path:
-            return settings.AIC_DOMAIN + path
+        if settings.AIC_BACKEND_DOMAIN not in path:
+            return settings.AIC_BACKEND_DOMAIN + path
         return path
