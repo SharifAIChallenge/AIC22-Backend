983cd4cb049711e6410c8113f743ba8b986e8b8e
komeil komeil <komeilyahyazade@gmail.com>
2022-07-03 15:29:44 +0000

    false the debug value

--- AIC22_Backend/settings.py
+++ AIC22_Backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ko^^x2))7s(n#ypjs+eakp^-#kf@ku=^07k!p8lfu4zl$w91o3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
