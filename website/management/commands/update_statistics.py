6e2e567224cdc50e2a6e6bc18ff62df54a911522
armita <armita@carriot.ir>
2022-07-06 21:04:40 +0430

    handle statistics management command

---
+++ website/management/commands/update_statistics.py
@@ -0,0 +1,19 @@
+
+
+from django.core.management import BaseCommand
+
+from django.apps import apps
+from website.models import Statistic
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        for statistic in Statistic.objects.all():
+            if statistic.model is None:
+                continue
+            model = apps.get_model(app_label='website', model_name=statistic.model)
+            model_counter = model.objects.count()
+            statistic.value = model_counter
+            statistic.save()
+
