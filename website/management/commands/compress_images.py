bd3f77a9682d59a8c27bc0db58744bcaef2df9a6
mohammad ebrahimi <mohammad.ebrahimi@divar.ir>
2022-07-08 02:12:50 +0430

    Add compress command

---
+++ website/management/commands/compress_images.py
@@ -0,0 +1,12 @@
+from django.core.management import BaseCommand
+
+from PIL import Image
+from website.models import Staff
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        for staff in Staff.objects.all():
+            image_path = staff.image.path
+            Image.open(image_path).convert("RGB").save(image_path)
