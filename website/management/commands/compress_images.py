6a9490ad184dcdac2c2d75e97ed8acc64fb28e6e
mohammad ebrahimi <mohammad.ebrahimi@divar.ir>
2022-07-08 02:13:28 +0430

    Set optimize true

--- website/management/commands/compress_images.py
+++ website/management/commands/compress_images.py
@@ -9,4 +9,4 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         for staff in Staff.objects.all():
             image_path = staff.image.path
-            Image.open(image_path).convert("RGB").save(image_path)
+            Image.open(image_path).convert("RGB").save(image_path, optimize=True)
