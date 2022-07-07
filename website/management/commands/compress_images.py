ba3e08fc021581b1683aeaeebeeae0bdab039604
mohammad ebrahimi <mohammad.ebrahimi@divar.ir>
2022-07-08 02:36:20 +0430

    Pass not found error

--- website/management/commands/compress_images.py
+++ website/management/commands/compress_images.py
@@ -8,5 +8,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         for staff in Staff.objects.all():
-            image_path = staff.image.path
-            Image.open(image_path).convert("RGB").save(image_path, optimize=True)
+            try:
+                image_path = staff.image.path
+                Image.open(image_path).convert("RGB").save(image_path, optimize=True)
+            except FileNotFoundError as error:
+                pass
