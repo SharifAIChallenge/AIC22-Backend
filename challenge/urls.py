7f3c6d6c82e8f67c007d70bcc35c591702233e31
Mohammad Broughani <gitmbroughani81@gmail.com>
2022-07-09 21:59:02 +0430

    add lobby to urls

--- challenge/urls.py
+++ challenge/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import SubmissionsListAPIView, SubmissionAPIView, TournamentAPIView, MatchAPIView
-
+from .views import (SubmissionsListAPIView, SubmissionAPIView, TournamentAPIView, MatchAPIView,
+                    LobbyAPIView)
 urlpatterns = [
     path('submission', view=SubmissionAPIView.as_view(), name='submission_api'),
     path('submissions', view=SubmissionsListAPIView.as_view(),
          name='submissions_list_api'),
     path('tournament', view=TournamentAPIView.as_view(), name='tournament'),
     path('match', view=MatchAPIView.as_view(), name='matches'),
+    path('lobby', view=LobbyAPIView.as_view(), name='lobby'),
 ]
