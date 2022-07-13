49b9aad9187401d7d1b7482eb29c27fb1c2cd4e6
mehrbod <mehrbodjavadi79@gmail.com>
2022-07-13 21:13:00 +0430

    fix code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python manage.py migrate sites
 python manage.py migrate
-python manage.py crontab add
 gunicorn --bind=0.0.0.0:8000 --timeout=90 --preload AIC22_Backend.wsgi:application
 
