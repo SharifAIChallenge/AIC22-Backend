662d5c3d156dd7564fe42af794080910b1febb4e
mehrbod <mehrbodjavadi79@gmail.com>
2022-07-13 18:39:16 +0430

    crontab for stage

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python manage.py migrate sites
 python manage.py migrate
+python manage.py crontab add
 gunicorn --bind=0.0.0.0:8000 --timeout=90 --preload AIC22_Backend.wsgi:application
 
