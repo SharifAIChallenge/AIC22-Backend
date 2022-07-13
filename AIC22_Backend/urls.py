22f5a65c59ccd40c929d006404207022b10cbb6a
mehrbod <mehrbodjavadi79@gmail.com>
2022-07-13 14:18:55 +0430

    remove extra code

--- AIC22_Backend/urls.py
+++ AIC22_Backend/urls.py
@@ -48,12 +48,3 @@ if not IS_PRODUCTION:
     urlpatterns += [
         path('api/v1/communication/', include('communication.urls')),
     ]
-
-if SENTRY_ENABLED:  # todo remove this after while
-    def trigger_error(request):
-        division_by_zero = 1 / 0
-
-
-    urlpatterns += [
-        path('sentry-debug/', trigger_error),
-    ]
