684508d7d596e370cf949737aae800809e80d079
ar-hosseinkhani <alireza.8096@chmail.ir>
2022-07-19 18:58:46 +0430

    handle specific login errors

--- account/urls.py
+++ account/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from account.views import SignUpAPIView, LoginAPIView, LogoutAPIView, ActivateAPIView, ProfileAPIView, \
     GoogleLoginAPIView, ChangePasswordAPIView, ResendActivationEmailAPIView, ResetPasswordAPIView, \
-    ResetPasswordConfirmAPIView, UserWithoutTeamAPIView
+    ResetPasswordConfirmAPIView, UserWithoutTeamAPIView, CustomLoginAPIView
 
 urlpatterns = [
 
     path('signup', view=SignUpAPIView.as_view(), name='signup'),
-    path('login', view=LoginAPIView.as_view(), name='login'),
+    path('login', view=CustomLoginAPIView.as_view(), name='login'),
     path('logout', view=LogoutAPIView.as_view(), name='logout'),
     path('activate/<slug:eid>/<slug:token>', view=ActivateAPIView.as_view(), name='activate'),
     path('profile', view=ProfileAPIView.as_view(), name='profile'),
