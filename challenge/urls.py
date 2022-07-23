6076679e32acbf93462db1edd68cbabb975471ad
mehrbod <mehrbodjavadi79@gmail.com>
2022-07-23 20:10:39 +0430

    fix code style

--- challenge/urls.py
+++ challenge/urls.py
@@ -5,8 +5,7 @@ from .views import (SubmissionsListAPIView, SubmissionAPIView, TournamentAPIView
 
 urlpatterns = [
     path('submission', view=SubmissionAPIView.as_view(), name='submission_api'),
-    path('submissions', view=SubmissionsListAPIView.as_view(),
-         name='submissions_list_api'),
+    path('submissions', view=SubmissionsListAPIView.as_view(), name='submissions_list_api'),
     path('tournament', view=TournamentAPIView.as_view(), name='tournament'),
     path('match', view=MatchAPIView.as_view(), name='matches'),
     path('lobby', view=LobbyAPIView.as_view(), name='lobby'),
@@ -14,11 +13,11 @@ urlpatterns = [
     path('request/<int:request_id>', view=RequestAPIView.as_view(), name='request'),
     # path('clan', view=ClanAPIView.as_view(), name='clan'),
     path('match', view=MatchAPIView.as_view(), name='matches'),
-    path('level_based_tournament', view=LevelBasedTournamentAPIView.as_view(),
-         name='level_based_tournament'),
-    path('level_based_tournament/add_teams',
-         view=LevelBasedTournamentAddTeamsAPIView.as_view(),
-         name='level_based_tournament_add_teams'),
-    path('scoreboard/<int:tournament_id>', view=ScoreboardAPIView.as_view(),
-         name='scoreboard'),
+    path('level_based_tournament', view=LevelBasedTournamentAPIView.as_view(), name='level_based_tournament'),
+    path(
+        'level_based_tournament/add_teams',
+        view=LevelBasedTournamentAddTeamsAPIView.as_view(),
+        name='level_based_tournament_add_teams'
+    ),
+    path('scoreboard/<int:tournament_id>', view=ScoreboardAPIView.as_view(), name='scoreboard'),
 ]
