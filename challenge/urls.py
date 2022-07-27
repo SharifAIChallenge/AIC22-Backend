c2972794fb5614d1550feadca3c980196d9c5935
mehrbod <mehrbodjavadi79@gmail.com>
2022-07-27 15:48:45 +0430

    add submission apis

--- challenge/urls.py
+++ challenge/urls.py
@@ -4,8 +4,8 @@ from .views import (SubmissionsListAPIView, SubmissionAPIView, TournamentAPIView
                     LevelBasedTournamentAddTeamsAPIView, ScoreboardAPIView)
 
 urlpatterns = [
-    # path('submission', view=SubmissionAPIView.as_view(), name='submission_api'),
-    # path('submissions', view=SubmissionsListAPIView.as_view(), name='submissions_list_api'),
+    path('submission', view=SubmissionAPIView.as_view(), name='submission_api'),
+    path('submissions', view=SubmissionsListAPIView.as_view(), name='submissions_list_api'),
     # path('tournament', view=TournamentAPIView.as_view(), name='tournament'),
     # path('match', view=MatchAPIView.as_view(), name='matches'),
     # path('lobby', view=LobbyAPIView.as_view(), name='lobby'),
