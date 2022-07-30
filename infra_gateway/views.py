f72b50c157b6ccf86097b4e4d34ab95dd47d1fd4
mehrbod <mehrbodjavadi79@gmail.com>
2022-07-31 01:53:14 +0430

    add hermes api

--- infra_gateway/views.py
+++ infra_gateway/views.py
@@ -14,6 +14,7 @@ class InfraEventPushAPIView(GenericAPIView):
         serializer = self.get_serializer(
             data=request.data
         )
+        print(request.data)
 
         serializer.is_valid(raise_exception=True)
 
