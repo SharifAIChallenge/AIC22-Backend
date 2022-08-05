3c2217403abeb0394a3d830fe2fa3d95ddd71123
mehrbod <mehrbodjavadi79@gmail.com>
2022-08-05 14:37:16 +0430

    add config_file to map model

--- challenge/models/map.py
+++ challenge/models/map.py
@@ -13,6 +13,11 @@ class Map(TimeStampedModel, UUIDModel):
     file = models.FileField(
         upload_to=settings.UPLOAD_PATHS["MAP"]
     )
+    config_file = models.FileField(
+        upload_to=settings.UPLOAD_PATHS["MAP"],
+        null=True,
+        blank=True,
+    )
     active = models.BooleanField(default=True)
     infra_token = models.CharField(
         max_length=LONG_TEXT_MAX_LENGTH,
@@ -22,7 +27,7 @@ class Map(TimeStampedModel, UUIDModel):
 
     def pre_save(self):
         from challenge.logics import upload_map
-        self.infra_token = upload_map(self.file)
+        self.infra_token = upload_map(self.file, self.config_file)
 
     def save(self, *args, **kwargs):
         self.pre_save()
