2f42d973ebd0f4503643a400d92dc21272114879
mohammadrezaesk <mohammad.1392.sk@gmail.com>
2022-08-31 10:46:47 +0430

    fix

--- account/admin.py
+++ account/admin.py
@@ -44,6 +44,7 @@ class ProfileAdmin(admin.ModelAdmin):
             data = [getattr(obj, field) for field in field_names[:21]]
             data += [obj.user.team.name if obj.user.team is not None else ""]
             data += [obj.user.email]
+            data += ["paid" if obj.is_paid else "not_yet"]
             row = writer.writerow(data)
 
         return response
