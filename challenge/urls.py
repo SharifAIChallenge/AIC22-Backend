e51e21289dc24724ac457e0f64f96c8fda3a05c9
mohammadrezaesk <mohammad.1392.sk@gmail.com>
2022-09-02 16:43:38 +0430

    close everythiiiing

--- challenge/urls.py
+++ challenge/urls.py
@@ -5,16 +5,16 @@ from .views import (SubmissionsListAPIView, SubmissionAPIView, TournamentAPIView
                     BotAPIView, TeamsWonBotAPIView)
 
 urlpatterns = [
-    path('submission', view=SubmissionAPIView.as_view(), name='submission_api'),
+    # path('submission', view=SubmissionAPIView.as_view(), name='submission_api'),
     path('submissions', view=SubmissionsListAPIView.as_view(), name='submissions_list_api'),
-    path('submission/<int:submission_id>', view=SubmissionAPIView.as_view(), name='update_submission'),
+    # path('submission/<int:submission_id>', view=SubmissionAPIView.as_view(), name='update_submission'),
     path('tournament', view=TournamentAPIView.as_view(), name='tournament'),
-    path('match', view=MatchAPIView.as_view(), name='matches'),
+    # path('match', view=MatchAPIView.as_view(), name='matches'),
     path('lobby', view=LobbyAPIView.as_view(), name='lobby'),
     # path('clan', view=ClanAPIView.as_view(), name='clan'),
     path('request', view=RequestListAPIView.as_view(), name='request_list'),
-    path('request/<int:request_id>', view=RequestAPIView.as_view(), name='request'),
-    path('match', view=MatchAPIView.as_view(), name='matches'),
+    # path('request/<int:request_id>', view=RequestAPIView.as_view(), name='request'),
+    # path('match', view=MatchAPIView.as_view(), name='matches'),
     path('level_based_tournament', view=LevelBasedTournamentAPIView.as_view(), name='level_based_tournament'),
     path(
         'level_based_tournament/add_teams',
